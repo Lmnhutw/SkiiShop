dc9788840336affc014f3bec448cbb04ea478aa7
DESKTOP-Q3QSGU0\lmnhut <lmnhut0211@gmail.com>
2025-07-05 23:23:20 +0700

    Add project files.

---
+++ SShopAPI/Program.cs
@@ -0,0 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddDbContext<DbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));  
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
